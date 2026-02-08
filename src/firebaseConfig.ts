6ce44e5a191dc0f99891b14756083b16774bccdc
DarcyCorbett <30725397+DarcyCorbett@users.noreply.github.com>
2026-02-08 21:30:52 +0200

    Create firebaseConfig.ts

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsaeEMTPyS33s6VJtnu0tTn9B9Hw0rTu4",
+  authDomain: "chefgenie-33661.firebaseapp.com",
+  projectId: "chefgenie-33661",
+  storageBucket: "chefgenie-33661.firebasestorage.app",
+  messagingSenderId: "580298101662",
+  appId: "1:580298101662:web:96605849edf736c0723c8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
